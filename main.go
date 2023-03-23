89aa0fb4bdbdd0046144fbec5cf59b5de30bebe6
Samuel Jakobsen <samuel@skaarhoj.com>
2023-03-23 09:41:32 +0100

    Comments in main as well

--- main.go
+++ main.go
@@ -10,21 +10,23 @@ const localhost = true
 const wifi = false
 
 func main() {
+	// enable line numbers in log
 	log.EnableLineNumbers()
 	//tsp_test()
 	getIp()
 	log.Info("Got IP: ", ip)
 
+	// this creates a channel and a routine for the key getter. Not used at the moment
 	keyChan := make(chan string)
 	go keyGet(keyChan)
 
+	// channels between visuals and robot server
 	commandChan := make(chan string)
 	poiChan := make(chan poiType)
 	go initVisualServer(poiChan, commandChan)
 	go initRobotServer(keyChan, poiChan, commandChan)
 
-	//go UI.ServerInit()
-
+	// loop main, can enable prints of routines to watch out for too many go routines
 	for {
 		time.Sleep(2 * time.Second)
 		//fmt.Println("goroutine: ", runtime.NumGoroutine())
